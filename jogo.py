f39d66faa74725f97e1a1c414ac593308f3c750b
Leonardo Walker <leonardowalker289@gmail.com>
2022-06-23 20:50:58 -0300

    jogo lisinho

--- jogo.py
+++ jogo.py
@@ -78,63 +78,63 @@ def game():
                     movimentoXNave = 0
                     movimentoYNave = 0
 
-            if gameplay == True:
-                posicaoXNave = posicaoXNave + movimentoXNave
-                if posicaoXNave < 0:
-                    posicaoXNave = 0
-                elif posicaoXNave > display_largura - larguraNave:
-                    posicaoXNave = display_largura - larguraNave
+        if gameplay == True:
+            posicaoXNave = posicaoXNave + movimentoXNave
+            if posicaoXNave < 0:
+                posicaoXNave = 0
+            elif posicaoXNave > display_largura - larguraNave:
+                posicaoXNave = display_largura - larguraNave
 
-                posicaoYNave = posicaoYNave + movimentoYNave
-                if posicaoYNave < 0:
-                    posicaoYNave = 0
-                elif posicaoYNave > display_altura - alturaNave:
-                    posicaoYNave = display_altura - alturaNave
+            posicaoYNave = posicaoYNave + movimentoYNave
+            if posicaoYNave < 0:
+                posicaoYNave = 0
+            elif posicaoYNave > display_altura - alturaNave:
+                posicaoYNave = display_altura - alturaNave
 
-                gameDisplay.fill(white)
-                gameDisplay.blit(background, (0, 0))
-                gameDisplay.blit(nave, (posicaoXNave, posicaoYNave))
-                gameDisplay.blit(
-                    meteoro, (movimentoXMeteoro, movimentoYMeteoro))
+            gameDisplay.fill(white)
+            gameDisplay.blit(background, (0, 0))
+            gameDisplay.blit(nave, (posicaoXNave, posicaoYNave))
+            gameDisplay.blit(
+                meteoro, (movimentoXMeteoro, movimentoYMeteoro))
+                
+            
+            if direcao == True:
+                if movimentoYMeteoro < 450 :
+                    movimentoYMeteoro = movimentoYMeteoro + velocidade
                     
                 
-                if direcao == True:
-                    if movimentoYMeteoro < 450 :
-                        movimentoYMeteoro = movimentoYMeteoro + velocidade
-                        
-                    
-                    else:
-                        movimentoYMeteoro = -185
-                        movimentoXMeteoro = random.randrange(0, display_largura-larguraMeteoro)
-                        velocidade = velocidade + 1
-                        pontos+=1
-                
-                fonte = pygame.font.Font('freesansbold.ttf', 20)
-                texto = fonte.render("Pontos: "+str(pontos), True, white)
-                gameDisplay.blit(texto, (20, 5))
-                vidas = fonte.render("Vidas: "+str(pontosMorrer), True, white)
-                gameDisplay.blit(vidas, (20, 30))
-
-       
-                #colisão        
-                naveRect = nave.get_rect()
-                naveRect.x = posicaoXNave
-                naveRect.y = posicaoYNave
-
-                meteoroRect = meteoro.get_rect()
-                meteoroRect.x = movimentoXMeteoro 
-                meteoroRect.y = movimentoYMeteoro-22
-
-                if naveRect.colliderect(meteoroRect) == True:
-                    gameplay = True
-                    pontosMorrer -=1
+                else:
                     movimentoYMeteoro = -185
-                    movimentoXMeteoro = random.randrange(0, display_largura)
-                if pontosMorrer == 0:
-                    perdeu(pontos)
-                    gameplay = False
-                else:    
-                    direcao = True
+                    movimentoXMeteoro = random.randrange(0, display_largura-larguraMeteoro)
+                    velocidade = velocidade + 1
+                    pontos+=1
+            
+            fonte = pygame.font.Font('freesansbold.ttf', 20)
+            texto = fonte.render("Pontos: "+str(pontos), True, white)
+            gameDisplay.blit(texto, (20, 5))
+            vidas = fonte.render("Vidas: "+str(pontosMorrer), True, white)
+            gameDisplay.blit(vidas, (20, 30))
+
+    
+            #colisão        
+            naveRect = nave.get_rect()
+            naveRect.x = posicaoXNave
+            naveRect.y = posicaoYNave
+
+            meteoroRect = meteoro.get_rect()
+            meteoroRect.x = movimentoXMeteoro 
+            meteoroRect.y = movimentoYMeteoro-22
+
+            if naveRect.colliderect(meteoroRect) == True:
+                gameplay = True
+                pontosMorrer -=1
+                movimentoYMeteoro = -185
+                movimentoXMeteoro = random.randrange(0, display_largura)
+            if pontosMorrer == 0:
+                perdeu(pontos)
+                gameplay = False
+            else:    
+                direcao = True
         print(pontos)    
 
         pygameDisplay.update()
